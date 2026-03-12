e688fbf4af82300ec15a5d6f52bba50063f91bff
Isabel Román <irm@us.es>
2026-03-12 12:46:43 +0100

    version spring initializer

---
+++ src/main/java/us/dit/ueba/openc2consumer/Openc2consumerApplication.java
@@ -0,0 +1,13 @@
+package us.dit.ueba.openc2consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openc2consumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openc2consumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/dit/ueba/openc2consumer/Openc2consumerApplicationTests.java
@@ -0,0 +1,13 @@
+package us.dit.ueba.openc2consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Openc2consumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
